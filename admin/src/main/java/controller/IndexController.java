225db78c49bbc53503eb7f400a3940468b2199ef
최주리 <@jrchoi>
2024-01-18 18:27:07 +0900

    index와 main 분리

--- admin/src/main/java/controller/IndexController.java
+++ admin/src/main/java/controller/IndexController.java
@@ -2,21 +2,19 @@ package controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import model.Brand;
-import service.BrandRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping(value="/admin")
 public class IndexController {
-	@Autowired
-	BrandRepository service;
-	@RequestMapping(value="/index")
-	public String index(){
-		return "index";
+
+	@RequestMapping(value="/main")
+	public String main(){
+		System.out.println("index controller1");
+		return "main";
 	}
 }
